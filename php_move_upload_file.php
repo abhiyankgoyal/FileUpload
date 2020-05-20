cf7134d82dcff552076988d53ae7c13597ad10e7
abhiyankgoyal <abhiyank0711@gmail.com>
2020-05-21 00:15:37 +0530

    file upload php javascript ajax

---
+++ php_move_upload_file.php
@@ -0,0 +1,28 @@
+<?php
+
+if(isset($_FILES['file']['name'])){
+   // file name
+   $filename = $_FILES['file']['name'];
+
+   // Location
+   $location = 'MyFiles/'.$filename;
+
+   // file extension
+   $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+   $file_extension = strtolower($file_extension);
+
+   // Valid image extensions
+   $valid_ext = array("pdf","doc","docx","jpg","png","jpeg");
+
+   $response = 0;
+   if(in_array($file_extension,$valid_ext)){
+      // Upload file
+      if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
+         $response = 1;
+      } 
+   }
+
+   echo $response;
+   exit;
+}
+?>
